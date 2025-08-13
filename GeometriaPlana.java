02d3a24c333f962bf99b62d46ef2adee787d9b53
João pedro <134340858+joaocidade@users.noreply.github.com>
2025-08-13 10:04:36 -0300

    retangulo

--- GeometriaPlana.java
+++ GeometriaPlana.java
@@ -3,4 +3,14 @@ public class GeometriaPlana extends Geometria {
     public double areaCirculo(double raio) {
         return pi * (raio * raio);
     }
+    public double perimetroCirculo(double raio){
+        return  2 * pi * raio;
+    }
+    public double areaRetangulo(double base, double altura){
+        return base * altura;
+    }
+    public double perimetroRetangulo(double base, double altura){
+        return  2 * (base + altura);
+    }  
 }
+
--- Main.java
+++ Main.java
@@ -22,10 +22,29 @@ public class Main {
                     double area_do_circulo = obj.areaCirculo(raio);
                     System.out.println(area_do_circulo);
                 } else if (escolhaUsuario2 == 2) {
-               
+                    double perimetroCirculo = obj.perimetroCirculo(raio);
+                    System.out.println(perimetroCirculo);
                 }
                 break;
-           
+            case 2:
+                System.out.println("Deseja qual opção: 1-area ou 2-perimetro?");
+                int escolhaUsuario3 = meuScanner.nextInt();
+
+                System.out.println("Qual e a base?");
+                double base = meuScanner.nextDouble();
+
+                System.out.println("Qual a altura?");
+                double altura = meuScanner.nextDouble();
+
+                if (escolhaUsuario3 == 1) {
+                    double areaDoRetangulo = obj.areaRetangulo(base, altura);
+                    System.out.println(areaDoRetangulo);
+                break;
+                } else if (escolhaUsuario3 == 2) {
+                    double  perimetroRetangulo = obj.perimetroRetangulo(base, altura);
+                    System.out.println(perimetroRetangulo);
+                }
+                break;
             default:
                 throw new AssertionError();
         }
